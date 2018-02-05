3d2ce4da73cc4c4faa8862b62cf893e131686417
rising <625687551@qq.com>
2018-02-05 08:29:59 +0800

    change merge sort

--- Python/merge_sort.py
+++ Python/merge_sort.py
@@ -2,19 +2,32 @@
 # -*- coding:utf-8 -*-
 
 
-def select_sort(lists):
-    # 直接排序
-    count = len(lists)
-    for i in range(count):
-        min = i
-        for j in range(i+1, count):
-            if lists[min] > lists[j]:
-                min = j
-            lists[min], lists[i] = lists[i], lists[min]
-    return lists
+def merge_sort(lists):
+    if len(lists) <= 1:
+        return lists
+    n = len(lists)
+    num = n//2
+    left = merge_sort(lists[:num])
+    right = merge_sort(lists[num:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
 
 
 if __name__ == '__main__':
     l = [29, 10, 15, 37, 14]
-    f = select_sort(l)
+    f = merge_sort(l)
     print("finally {}".format(f))
